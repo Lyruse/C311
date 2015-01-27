64c37adad6cbecf360c35b8a24a021971f9b7d28
lyruse <hhj9010@gmail.com>
2015-01-28 00:03:42 +0800

    Finish A12 perfectly.

--- a12.rkt
+++ a12.rkt
@@ -9,8 +9,12 @@
 
 #lang racket
 (require C311/monads)
+(require C311/pmatch)
 #;(require C311/a12-student-tests)
 #;(test-file #:file-name "a12.rkt")
+; test result:
+
+;  34 success(es) 0 failure(s) 0 error(s) 34 test(s) run
 #;#;#;
 (define return-maybe
   (lambda (a) `(Just ,a)))
@@ -277,3 +281,170 @@
 ; ((traverse-state/sum '((1 . 2) . (3 . (4 . 5)))) 0)
 ;=>(((0 . 1) 3 6 . 10) . 15)
 ;----------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+;----------------------------- CPS Monad Interpreter -----------------------------
+(define empty-env (lambda () '()))
+(define extend-env (lambda (x v env) `((,x . ,v) . ,env)))
+(define apply-env (lambda (env x) (pmatch env
+                                    [`((,x^ . ,v) . ,env)
+                                     (if (eq? x x^)
+                                         v
+                                         (apply-env env x))])))
+(define closure (lambda (arg body env)
+                  `(,arg ,body ,env)))
+(define apply-proc (lambda (clo val)
+                     (pmatch clo
+                       [`(,arg ,body ,env)
+                        (value-of body (extend-env arg val env))])))
+(define value-of
+  (lambda (expr env)
+    (pmatch expr
+      [`,n (guard (or (number? n) (boolean? n))) n]
+      [`,x (guard (symbol? x)) (apply-env env x)]
+      [`(* ,x1 ,x2) (* (value-of x1 env) (value-of x2 env))]
+      [`(sub1 ,x) (sub1 (value-of x env))]
+      [`(zero? ,x) (zero? (value-of x env))]
+      [`(if ,test ,conseq ,alt) (if (value-of test env)
+                                    (value-of conseq env)
+                                    (value-of alt env))]
+      [`(capture ,k-id ,body) (call/cc (lambda (k)
+                                         (value-of body (extend-env k-id k env))))]
+      [`(return ,v-exp ,k-exp) ((value-of k-exp env) (value-of v-exp env))]
+      [`(lambda (,id) ,body) (closure id body env)]
+      [`(,rator ,rand) (apply-proc (value-of rator env) (value-of rand env))])))
+
+
+
+
+
+
+(define apply-proc-cps (lambda (clo val)
+                     (pmatch clo
+                       [`(,arg ,body ,env)
+                        (value-of-cps body (extend-env arg val env))])))
+(define value-of-cps
+  (lambda (expr env)
+    (pmatch expr
+      [`,n (guard (or (number? n) (boolean? n))) (return-cont n)]
+      [`,x (guard (symbol? x)) (return-cont (apply-env env x))]
+      [`(* ,x1 ,x2) (bind-cont (value-of-cps x1 env)
+                               (lambda (x1)
+                                 (bind-cont (value-of-cps x2 env)
+                                            (lambda (x2)
+                                              (return-cont (* x1 x2))))))]
+      [`(sub1 ,x) (do bind-cont 
+                    (x <- (value-of-cps x env))
+                    (return-cont (sub1 x)))]
+      [`(zero? ,x) (do bind-cont 
+                    (x <- (value-of-cps x env))
+                    (return-cont (zero? x)))]
+      [`(if ,test ,conseq ,alt) (do bind-cont 
+                                  (res <- (value-of-cps test env))
+                                  (if res
+                                      (value-of-cps conseq env)
+                                      (value-of-cps alt env)))]
+      [`(capture ,k-id ,body) (callcc (lambda (out)
+                                        (value-of-cps body (extend-env k-id out env))))]
+      [`(return ,v-exp ,k-exp) (do bind-cont
+                                 (k <- (value-of-cps k-exp env))
+                                 (a <- (value-of-cps v-exp env))
+                                 (k a))]
+      [`(lambda (,id) ,body) (return-cont (closure id body env))]
+      [`(,rator ,rand) (do bind-cont
+                         (r <- (value-of-cps rator env))
+                         (d <- (value-of-cps rand env))
+                         (apply-proc-cps r d))])))
+
+(define fact-5   ;; for test
+  '((lambda (f)
+      ((f f) 5))
+    (lambda (f)
+      (lambda (n)
+        (if (zero? n)
+            1
+            (* n ((f f) (sub1 n))))))))
+;> ((value-of-cps fact-5 (empty-env)) (lambda (v) v))
+;120
+(define capture-fun  ;; for test
+  '(* 3 (capture q (* 2 (return 4 q)))))
+;> ((value-of-cps capture-fun (empty-env)) (lambda (v) v))
+;12
+
+
+
+#|
+Just Dessert: The Fringe
+Like baking soda, the traverse function above has many applications. Here's one more.
+ We're going to use the Cont monad to achieve a kind of trampolining or multitasking.
+
+The fringe of a tree is the list of its leaves in left-to-right order. For example, 
+the fringe of
+
+(("Time" . "flies") . ("like" . ("an" . "arrow")))
+is the list of strings
+
+("Time" "flies" "like" "an" "arrow")
+Given two trees, the driver function compares their fringes 
+while ignoring any differences in capitalization. If the two fringes are the same, 
+then driver returns the two trees (in a list) with their leaves swapped.
+ If the two fringes have bigger differences than mere capitalization, 
+then driver returns #f.
+
+For example, here are two tests:
+
+ 
+> (driver '(("Time" . "flies") . ("like" . ("an" . "arrow")))
+	  '("time" . ("FLIES" . (("like" . "an") . "aRrOw"))))
+((("time" . "FLIES") . ("like" . ("an" . "aRrOw")))
+ ("Time" . ("flies" . (("like" . "an") . "arrow"))))
+ 
+> (driver '(("Time" . "flies") . ("like" . ("arrow" . "an")))
+          '("time" . ("FLIES" . (("like" . "an") . "aRrOw"))))
+#f
+Below is most of the definition of driver. Your mission is
+ to complete the definition by defining yield-cont.
+|#
+
+(define exchange
+  (lambda ($1 $2)
+    (pmatch `(,$1 ,$2)
+      [`((stopped ,tree1) (stopped ,tree2))
+       `(,tree1 ,tree2)]
+      [`((yielding ,x1 ,k1) (yielding ,x2 ,k2))
+       (guard (string-ci=? x1 x2))
+       (exchange (k1 x2) (k2 x1))]
+      [else #f])))
+#;
+(define traverse
+  (lambda (return bind f)
+    (letrec
+        ((trav
+          (lambda (tree)
+            (cond
+              [(pair? tree)
+               (do bind
+                 (a <- (trav (car tree)))
+                 (d <- (trav (cdr tree)))
+                 (return (cons a d)))]
+              [else (f tree)]))))
+      trav)))
+(define tree->stream
+  (lambda (tree)
+    (((traverse return-cont bind-cont yield-cont)
+      tree)
+     (lambda (tree) `(stopped ,tree)))))
+(define driver
+  (lambda (tree1 tree2)
+    (exchange (tree->stream tree1) (tree->stream tree2))))
+
+(define yield-cont      ;; it seems a bit too easy.
+  (lambda (leave)
+    (lambda (k)
+      `(yielding ,leave ,k))))
